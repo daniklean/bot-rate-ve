a879d9fe445633894f1ac1adae1173d6c47b8027
daniklean <danielvieras10@gmail.com>
2022-10-20 17:53:23 -0400

    I added command /help and restore to /start

--- app/app.ts
+++ app/app.ts
@@ -15,7 +15,8 @@ import {
 const bot = new Telegraf<Context<Update>>(process.env.TOKEN as string)
 
 bot.start((ctx:Context) => {
-  ctx.reply('Iniciado bot')
+  const start = '¿Deseas obtener las cotizaciones más importantes en Venezuela?.\n\nIndicame que cotización quieres observar por medio de mis comandos, para más información de ellos enviame /help. '
+  ctx.reply(start)
 })
  
 bot.command('/average', async (ctx:Context) => {
@@ -56,6 +57,11 @@ bot.command('/airtm', async (ctx:Context) => {
   console.log(await ctx.reply(`Colateralización: ${quoteRate?.pair}\nExchange: ${quoteRate?.name}\nPrecio: ${quoteRate?.quote}\nFecha de Actualización: ${quoteRate?.dateLocal}`, chatId))
 }) 
 
+bot.help((ctx:Context) => {
+  const chatId:any = ctx.chat?.id
+  const helpString = 'Tienes la lista de comandos\n\n/average: Promedio Chigüire Rates\n/yadio: Promedio de Yadio.io\n/bcv: Promedio del Banco Central de Venzuela\n/airtm: Promedio de airtm (Mercado)\n/global66: Promedio de Globall 66\n/dolartoday: Promedio de DolarToday'
+  ctx.reply(`${helpString}`, chatId) 
+})
 const launchNow = async () => {
   try {
     const botActive = await bot.launch() 
